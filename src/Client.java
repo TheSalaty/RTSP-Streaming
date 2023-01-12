823649aaba7c35979859887973922f860efad438
Salaty <TheSalaty@gmail.com>
2023-01-12 17:51:58 +0100

    fix: video

--- src/Client.java
+++ src/Client.java
@@ -156,7 +156,7 @@ public class Client {
     // inputPanel.setSize(620,50);
 
     f.getContentPane().add(mainPanel, BorderLayout.CENTER);
-    f.setSize(new Dimension(640, 800));
+    f.setSize(new Dimension(660, 870));
     f.setVisible(true);
 
     // init timer
@@ -224,41 +224,6 @@ public class Client {
       logger.log(Level.INFO, "Setup Button pressed !");
 
       if (state == INIT) {
-        // Init non-blocking RTPsocket that will be used to receive data
-        try {
-          // TASK construct a new DatagramSocket to receive server RTP packets on port
-          // RTP_RCV_PORT
-          RTPsocket = new DatagramSocket(RTP_RCV_PORT);
-
-          // for now FEC packets are received via RTP-Port, so keep comment below
-          // FECsocket = new DatagramSocket(FEC_RCV_PORT);
-
-          // TASK set Timeout value of the socket to 1 ms
-          RTPsocket.setSoTimeout(1);
-          // ....
-          System.out.println(
-              "Socket receive buffer: " + RTPsocket.getReceiveBufferSize());
-
-          rtpHandler.setFecDecryptionEnabled(checkBoxFec.isSelected());
-
-          // Init the FEC-handler
-          fec = new FecHandler(checkBoxFec.isSelected());
-          // Init the play timer
-          int timerDelay = FRAME_RATE; // use default delay
-          if (framerate != 0) { // if information available, use that
-            timerDelay = 1000 / framerate; // delay in ms
-          }
-          timerPlay = new Timer(timerDelay, new timerPlayListener());
-          timerPlay.setCoalesce(true); // combines events
-          // timerPlay.setInitialDelay(0);
-
-        } catch (SocketException se) {
-          System.out.println("Socket exception: " + se);
-          System.exit(0);
-        }
-
-        VideoFileName = textField.getText();
-
         if (framerate == 0) { // no information on media available
           send_RTSP_request("DESCRIBE");
           if (parse_server_response() != 200) { // block and wait for response
@@ -283,6 +248,9 @@ public class Client {
               "Socket receive buffer: " + RTPsocket.getReceiveBufferSize());
 
           rtpHandler.setFecDecryptionEnabled(checkBoxFec.isSelected());
+
+          // FEC-Handler
+          fec = new FecHandler(checkBoxFec.isSelected());
           // Init the play timer
           int timerDelay = FRAME_RATE; // use default delay
           if (framerate != 0) { // if information available, use that
@@ -290,6 +258,7 @@ public class Client {
           }
           timerPlay = new Timer(timerDelay, new timerPlayListener());
           timerPlay.setCoalesce(true); // combines events
+
           // timerPlay.setInitialDelay(0);
 
         } catch (SocketException se) {
@@ -309,8 +278,11 @@ public class Client {
         } else {
           // TASK change RTSP state and print new state to console and statusLabel
           state = READY;
-          statusLabel.setText("READY");
+          statusLabel.setText(Integer.toString(READY));
           logger.log(Level.INFO, "New RTSP state: READY\n");
+          // state = ....
+          // statusLabel
+          // logger.log(Level.INFO, "New RTSP state: \n");
         }
       } // else if state != INIT then do nothing
     }
@@ -318,7 +290,6 @@ public class Client {
 
   /** Handler for Play button */
   class playButtonListener implements ActionListener {
-
     public void actionPerformed(ActionEvent e) {
       Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
 
@@ -327,6 +298,7 @@ public class Client {
         // TASK increase RTSP sequence number
         // .....
         RTSPSeqNb++;
+        // .....
 
         // Send PLAY message to the server
         send_RTSP_request("PLAY");
@@ -338,8 +310,11 @@ public class Client {
           // TASK change RTSP state and print out new state to console an statusLabel
           // state = ....
           state = PLAYING;
-          statusLabel.setText("PLAY");
-          logger.log(Level.INFO, "New RTSP state: PLAY\n");
+          statusLabel.setText(Integer.toString(PLAYING));
+          logger.log(Level.INFO, "New RTSP state: PLAYING\n");
+
+          // state = ....
+
           // start the timer
           timer.start();
           timerPlay.start();
@@ -350,7 +325,6 @@ public class Client {
 
   /** Handler for Pause button */
   class pauseButtonListener implements ActionListener {
-
     public void actionPerformed(ActionEvent e) {
       Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
 
@@ -359,6 +333,7 @@ public class Client {
         // TASK increase RTSP sequence number
         // ....
         RTSPSeqNb++;
+        // ....
 
         // Send PAUSE message to the server
         send_RTSP_request("PAUSE");
@@ -370,8 +345,9 @@ public class Client {
           // TASK change RTSP state and print out new state to console and statusLabel
           // state = ....
           state = READY;
-          statusLabel.setText("READY");
+          statusLabel.setText(Integer.toString(READY));
           logger.log(Level.INFO, "New RTSP state: READY\n");
+          // state = ....
 
           // stop the timer
           timer.stop();
@@ -385,7 +361,6 @@ public class Client {
 
   /** Handler for Teardown button */
   class tearButtonListener implements ActionListener {
-
     public void actionPerformed(ActionEvent e) {
       Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
 
@@ -403,8 +378,9 @@ public class Client {
         // TASK change RTSP state and print out new state to console and statusLabel
         // state = ....
         state = INIT;
-        statusLabel.setText("INIT");
+        statusLabel.setText(Integer.toString(INIT));
         logger.log(Level.INFO, "New RTSP state: INIT\n");
+        // state = ....
 
         // stop the timer
         timer.stop();
@@ -419,7 +395,6 @@ public class Client {
 
   /** Handler for Options button */
   class optionsButtonListener implements ActionListener {
-
     public void actionPerformed(ActionEvent e) {
       Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
 
@@ -435,7 +410,6 @@ public class Client {
 
   /** Handler for Describe button */
   class describeButtonListener implements ActionListener {
-
     public void actionPerformed(ActionEvent e) {
       Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
 
@@ -453,7 +427,6 @@ public class Client {
    * Handler for the timer event fetches the RTP-packets and displays the images
    */
   class timerListener implements ActionListener {
-
     byte[] buf = new byte[MAX_FRAME_SIZE]; // allocate memory to receive UDP data from server
 
     public void actionPerformed(ActionEvent e) {
@@ -483,6 +456,7 @@ public class Client {
       byte[] payload;
 
       // check buffer size and start if filled
+
       int puffer = rs.latestSequenceNumber - rs.playbackIndex;
       progressBuffer.setValue(puffer);
       progressPosition.setValue(rs.playbackIndex);
@@ -503,9 +477,7 @@ public class Client {
         return;
       }
 
-      logger.log(
-          Level.FINE,
-          "----------------- Play timer --------------------");
+      logger.log(Level.FINE, "----------------- Play timer --------------------");
       payload = rtpHandler.nextPlaybackImage();
       if (payload == null) {
         return;
@@ -519,6 +491,7 @@ public class Client {
         // display the image as an ImageIcon object
         icon = new ImageIcon(image);
         iconLabel.setIcon(icon);
+
       } catch (RuntimeException rex) {
         rex.printStackTrace();
       }
@@ -598,26 +571,30 @@ public class Client {
           case "cseq:":
             logger.log(Level.FINE, "SNr: " + headerField.nextToken());
             break;
+
           case "session:":
             if (state == INIT) {
               RTSPid = headerField.nextToken().split(";")[0]; // cat semicolon
             }
             break;
+
           case "content-length:":
             cl = Integer.parseInt(headerField.nextToken());
             break;
+
           case "public:":
-            logger.log(
-                Level.INFO,
-                "Options-Response: " + headerField.nextToken());
+            logger.log(Level.INFO, "Options-Response: " + headerField.nextToken());
             break;
+
           case "content-type:":
             String ct = headerField.nextToken();
             logger.log(Level.INFO, "Content-Type: " + ct);
             break;
+
           case "transport:":
             logger.log(Level.INFO, "");
             break;
+
           default:
             logger.log(Level.INFO, "Unknown: " + line);
         }
@@ -627,6 +604,7 @@ public class Client {
       // Describe will send content
       if (cl > 0)
         parse_server_data(cl);
+
     } catch (Exception ex) {
       ex.printStackTrace();
       logger.log(Level.SEVERE, "Exception caught: " + ex);
@@ -679,10 +657,12 @@ public class Client {
       // TASK Complete the RTSP request method line
       // rtspReq = ....
       rtspReq = request_type + " " + rtsp + " RTSP/1.0" + CRLF;
+      // rtspReq = ....
 
       // TASK write the CSeq line:
       // rtspReq += ....
       rtspReq += "CSeq: " + RTSPSeqNb + CRLF;
+      // rtspReq += ....
 
       // check if request_type is equal to "SETUP" and in this case write the
       // Transport: line
@@ -690,14 +670,8 @@ public class Client {
       // RTP_RCV_PORT
       // otherwise, write the Session line from the RTSPid field
       if (request_type.equals("SETUP")) {
-        // TASK Complete the Transport Attribute
-        // rtspReq += "Transport:";
-        rtspReq += "Transport: RTP/AVP;unicast;client_port=" +
-            RTP_RCV_PORT +
-            "-" +
-            RTP_RCV_PORT +
-            1 +
-            CRLF;
+        // TASK_x Complete the Transport Attribute
+        rtspReq += "Transport: RTP/AVP;unicast;client_port=" + RTP_RCV_PORT + "-" + RTP_RCV_PORT + 1 + CRLF;
       }
 
       // SessionIS if available
@@ -710,6 +684,7 @@ public class Client {
       RTSPBufferedWriter.write(rtspReq + CRLF);
       RTSPBufferedWriter.flush();
       logger.log(Level.INFO, "*** RTSP-Request " + request_type + " send ***");
+
     } catch (Exception ex) {
       ex.printStackTrace();
       logger.log(Level.SEVERE, "Exception caught: " + ex);
